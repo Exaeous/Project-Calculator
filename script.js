30d7f02bb8cb9d86dfbf42e1c29fe6aed64f386b
Samuel <samuelpulukuri@outlook.com>
2024-01-23 00:10:23 +0530

    Add basic math operations

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// Basic Operations
+
+// Addition
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+// Subtraction
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+//Multiplication
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+
+//Division
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
