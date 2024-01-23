397c71a7dbc4f2dcb9fd192744824d4b4ba383c4
Samuel <samuelpulukuri@outlook.com>
2024-01-23 20:45:54 +0530

    Add operation logic and refactor code

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
 "use strict";
 
+// let firstNumber = 2;
+// let secondNumber = 4;
+// let operator = "+";
+
 // Basic Operations
 
 // Addition
-function add(numberOne, numberTwo) {
-  return numberOne + numberTwo;
+function add(a, b) {
+  return a + b;
 }
 
 // Subtraction
-function subtract(numberOne, numberTwo) {
-  return numberOne - numberTwo;
+function subtract(a, b) {
+  return a - b;
 }
 
 //Multiplication
-function multiply(numberOne, numberTwo) {
-  return numberOne * numberTwo;
+function multiply(a, b) {
+  return a * b;
 }
 
 //Division
-function divide(numberOne, numberTwo) {
-  return numberOne / numberTwo;
+function divide(a, b) {
+  return a / b;
 }
+
+//Operation
+function operation(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+    default:
+      return "Error";
+  }
+}
+
+console.log(operation(2, 2, "/"));
