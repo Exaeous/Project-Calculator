2dc4f0deee7704c36644be77471fea8e2f41fe15
Sam <exaeous@gmail.com>
2024-06-12 19:03:03 +0530

    Add logic for populating number

--- script.js
+++ script.js
@@ -2,7 +2,20 @@
 
 let firstNumber = 2;
 let secondNumber = 4;
-let operator = "*";
+let operator = "+";
+
+let displayValue = 0;
+
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
+const equalButton = document.querySelector(".equal-btn");
+const displayArea = document.querySelector(".result");
+
+numberButtons.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", () => {
+    displayArea.textContent += numberBtn.textContent;
+  });
+});
 
 function add(a, b) {
   return a + b;
