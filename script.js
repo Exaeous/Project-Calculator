a061952f32e7f311ab7920f59d43387ce25c578b
Sam <exaeous@gmail.com>
2024-06-12 23:55:57 +0530

    Add logic to get first, second number and operator and refactor code

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 "use strict";
 
-let firstNumber = 2;
-let secondNumber = 4;
-let operator = "*";
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
 const numberButtons = document.querySelectorAll(".number-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
 const equalButton = document.querySelector(".equal-btn");
-const displayArea = document.querySelector(".result");
+const result = document.querySelector(".result");
+
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", () => {
+    if (operator === "") {
+      firstNumber += numberButton.textContent;
+      result.textContent += numberButton.textContent;
+    } else {
+      secondNumber += numberButton.textContent;
+      result.textContent += numberButton.textContent;
+    }
+  });
+});
+
+operatorButtons.forEach((operatorButton) => {
+  operatorButton.addEventListener("click", () => {
+    operator = operatorButton.textContent;
+    result.textContent += operator;
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  operate(firstNumber, secondNumber, operator);
+});
 
 function operate(firstNumber, secondNumber, operator) {
   switch (operator) {
     case "+":
-      console.log(firstNumber + secondNumber);
+      result.textContent = parseFloat(firstNumber) + parseFloat(secondNumber);
       break;
     case "-":
-      console.log(firstNumber - secondNumber);
+      result.textContent = parseFloat(firstNumber) - parseFloat(secondNumber);
       break;
     case "*":
-      console.log(firstNumber * secondNumber);
+      result.textContent = parseFloat(firstNumber) * parseFloat(secondNumber);
       break;
     case "/":
-      console.log(firstNumber / secondNumber);
+      result.textContent = parseFloat(firstNumber) / parseFloat(secondNumber);
       break;
     default:
       console.log("Invalid Operation");
   }
 }
-operate(firstNumber, secondNumber, operator);
