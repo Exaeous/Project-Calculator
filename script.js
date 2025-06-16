5a528803f64477bd29f48feb1afaff9ae919a872
Sam <exaeous@gmail.com>
2025-06-16 19:42:38 +0530

    Add functions for basic math operations

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// Addtion
+function add(a, b) {
+  return a + b;
+}
+
+// Subtraction
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiplication
+function multiply(a, b) {
+  return a * b;
+}
+
+// Division
+function divide(a, b) {
+  return a / b;
+}
